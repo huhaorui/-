e98af5eb1378328a2c3dd92132b4d402a0467f48
huhaorui <43870434+huhaorui@users.noreply.github.com>
2019-05-22 15:47:40 +0800

    保存

--- Project1/date.cpp
+++ Project1/date.cpp
@@ -7,11 +7,40 @@ date::date(int y, int m, int d)
 	month = m;
 	day = d;
 }
+bool operator >(date& d1, date& d2)
+{
+	if (d1.year > d2.year)
+		return true;
+	else if (d1.year < d2.year)
+		return false;
+	else if (d1.month > d2.month)
+		return true;
+	else if (d1.month < d2.month)
+		return false;
+	else if (d1.day > d2.day)
+		return true;
+	else return false;
+}
+bool operator <(date& d1, date& d2)
+{
+	if (d1.year < d2.year)
+		return true;
+	else if (d1.year > d2.year)
+		return false;
+	else if (d1.month < d2.month)
+		return true;
+	else if (d1.month > d2.month)
+		return false;
+	else if (d1.day < d2.day)
+		return true;
+	else return false;
+}
 ostream& operator <<(ostream& output, date& d)
 {
 	output << d.year << " " << d.month << " " << d.day << " ";
 	return output;
 }
+
 
 
 
